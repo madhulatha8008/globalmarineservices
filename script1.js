c81fbbc0f61e804b5ce41b9e3222c04c46f4a754
madhulatha8008 <madhupodilapu999@gmail.com>
2025-05-27 00:07:48 +0530

    Updated HTML, CSS, JS files with new functionality

--- script1.js
+++ script1.js
@@ -154,6 +154,11 @@ if (leftBtn && rightBtn) {
   });
 }
 
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
 
-
-
+if (hamburger && navMenu) {
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+  });
+}
